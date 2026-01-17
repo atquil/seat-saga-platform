b126d2e8e8ab1bcc11aa7fb5b726225d5e15edad
ATQUIL <creator.atquil@gmail.com>
2026-01-17 22:12:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# seat-saga-platform
---
+++ src/test/java/com/atquil/seatsagaplatform/SeatSagaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atquil.seatsagaplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatSagaPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
